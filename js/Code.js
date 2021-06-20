f12db6df6f9873aa5096f191a7c5d4980711dcd3
reemasifkhandey <73415822+reemasifkhandey@users.noreply.github.com>
2021-06-20 19:05:32 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    // add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,180)
+    fill("lightblue")
+    text("Hint: A set of statements that perfoms tasks !!", 100,200)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,350)
+    fill("lightblue")
+    text("Hint: Collection of information !!", 100,380)
+}
